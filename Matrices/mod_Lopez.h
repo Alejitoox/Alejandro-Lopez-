eab41a4ce10bcef152860385ac00d1c1a6d6da39
Alejitoox <alobeps4@gmail.com>
2025-09-24 22:20:17 -0500

    Delete Matrices directory

--- Matrices/mod_Lopez.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MODULO_H__
-#define __MODULO_H__
-
-//firmas de funciones del codigo
-void iniMatrix(int dim, double *matrix);
-void impMatriz(int N, double *m);
-void multMM(int N, const double *A, const double *B, double *C);
-
-#endif
