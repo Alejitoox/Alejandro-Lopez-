63de79802b768f29875ba57347ff0bf3116752e0
Alejitoox <alobeps4@gmail.com>
2025-11-09 00:55:35 -0500

    Update mmClasicaOpenMP.h

--- TallerRendimiento/ClasicaOpenMP/mmClasicaOpenMP.h
+++ TallerRendimiento/ClasicaOpenMP/mmClasicaOpenMP.h
@@ -1,6 +1,6 @@
 /****************************************************************************************
 * Fecha: 03-Nov-2025
-* Autor: David Calderon y Alejandro Lopez
+* Autor: Alejandro Lopez - David Calderon 
 * Materia: Sistemas Operativos
 * Tema: Taller de Evaluación de Rendimiento
 * Fichero: Interfaz (header) para mmClasicaOpenMP
